e2237497116e699fd1a16fd26d2304cd1d0062dd
Adam Tyihak <51171007+OldenErwanin@users.noreply.github.com>
2021-08-10 18:26:45 +0200

    Update Datasource.java

--- java/sample/musicdatabase/model/Datasource.java
+++ java/sample/musicdatabase/model/Datasource.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Datasource {
     public static final String DB_NAME = "music.db";
-    public static final String CONNECTION_STRING = "jdbc:sqlite:C:\\Users\\OldenErwanin\\JavaDevelopment\\DatabasePractice\\" + DB_NAME;
+    public static final String CONNECTION_STRING = "jdbc:sqlite:C:\\Users\\Anonymous\\JavaDevelopment\\DatabasePractice\\" + DB_NAME;
 
     public static final String TABLE_ALBUMS = "albums";
     public static final String COLUMN_ALBUM_ID = "_id";
